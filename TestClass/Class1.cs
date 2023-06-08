fb7ba80cf0b0e162b7c463b9c65d36870412b3e6
Smith Dabre <sdabre5954@conestogac.on.ca>
2023-06-07 23:53:35 -0400

    Added test cases for IsoscelesTriangle

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -24,6 +24,52 @@ namespace TestClass
             Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
         }
 
-        
+        [Test]
+        public void isIsoscelesTriangle1()
+        {
+            // Arrange 
+            int side1, side2, side3;
+            side1 = 5;
+            side2 = 5;
+            side3 = 3;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+        }
+
+        [Test]
+        public void isIsoscelesTriangle2()
+        {
+            // Arrange 
+            int side1, side2, side3;
+            side1 = 7;
+            side2 = 5;
+            side3 = 7;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+        }
+
+        [Test]
+        public void isIsoscelesTriangle3()
+        {
+            // Arrange 
+            int side1, side2, side3;
+            side1 = 6;
+            side2 = 9;
+            side3 = 9;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid and is an ISOSCELES", result);
+        }
     }
 }
