e6a9644b2eca42bf8648faaf1b0cc9ad5a8367bf
Smith Dabre <sdabre5954@conestogac.on.ca>
2023-06-07 23:43:43 -0400

    Added testcase for EquilateralTriangle

---
+++ TestClass/Class1.cs
@@ -0,0 +1,29 @@
+﻿using TriangleSolver;
+using System;
+using NUnit.Framework;
+
+namespace TestClass
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public void isEquilateralTriangle()
+        {
+            // Arrange 
+            int side1, side2, side3;
+            side1 = 8;
+            side2 = 8;
+            side3 = 8;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            Assert.AreEqual("The triangle is valid and is an EQUILATERAL", result);
+        }
+
+        
+    }
+}
